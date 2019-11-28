754d51344be1a68db7ed42942455c5ed91b617bf
wAuner <venice.venicebeach@icloud.com>
2019-11-28 13:52:42 +0100

    first test

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/ubw9nckwmqm8bz5/catsdogsmodel.pkl?dl=1'
+export_file_name = 'catsdogsmodel.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['cat', 'dog']
 path = Path(__file__).parent
 
 app = Starlette()
